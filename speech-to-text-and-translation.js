2f962c59c0e5498d3765d46c8ad0f67984256579
Ivaylo Toskov <kennelcrash@gmail.com>
2015-05-12 17:32:05 +0200

    Speech synthesis functionality added

--- speech-to-text-and-translation.js
+++ speech-to-text-and-translation.js
@@ -7,6 +7,9 @@ var imageWrapper = document.getElementById('input-img-wrapper');
 var image = document.getElementById('input-img');
 var inputText = document.getElementById('input-text-area');
 var inputLanguageSelect = document.getElementById('input-select');
+var outputLanguageSelect = document.getElementById('output-select');
+var outputText = document.getElementById('output-text-area');
+var outputImg = document.getElementById('output-img');
 
 if (window.SpeechRecognition === null) {
 	alert('No speech recognition available');
@@ -18,16 +21,25 @@ if (window.SpeechRecognition === null) {
         recognizer.continuous = true;
  
         // Start recognising
-        recognizer.onresult = function(event) {
-          inputText.textContent = '';
+	recognizer.onresult = function(event) {
+        	inputText.textContent = '';
+	  	outputText.textContent = '';
  
-          for (var i = event.resultIndex; i < event.results.length; i++) {
-            if (event.results[i].isFinal) {
-              inputText.textContent = event.results[i][0].transcript;
-            } else {
-              inputText.textContent += event.results[i][0].transcript;
-            }
-          }
+          	for (var i = event.resultIndex; i < event.results.length; i++) {
+            		if (event.results[i].isFinal) {
+		      		inputText.textContent = event.results[i][0].transcript;
+		      		outputText.textContent = event.results[i][0].transcript;
+            		} else {
+		      		inputText.textContent += event.results[i][0].transcript;
+		      		outputText.textContent += event.results[i][0].transcript;
+        		}
+		}
+
+		  var msg = new SpeechSynthesisUtterance(outputText.textContent);
+		  msg.lang = outputLanguageSelect.options[ inputLanguageSelect.selectedIndex ].value;
+		  outputImg.setAttribute('class','show');
+	    	  window.speechSynthesis.speak(msg);
+		  hideImg(outputText.textContent.length);
         };
 
 	// Listen for errors
@@ -52,4 +64,12 @@ if (window.SpeechRecognition === null) {
 			recognizer.stop();
 		}
         });
+}
+
+// Function for hiding the ouput indication image with
+// approximatively calculated timeout
+function hideImg(outputLength) {
+	setTimeout(function () {
+        	outputImg.setAttribute('class','hidden');
+    	}, outputLength*80);
 }
